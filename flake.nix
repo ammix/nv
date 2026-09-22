f06b4e974a48669190fda44db0f1b57ca60da2be
ammix <maxim@ammix.dev>
2026-09-22 20:43:28 +0200

    flake: bump version to 0.2.0

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     {
       packages.x86_64-linux.default = pkgs.buildGoModule {
         pname = "nv";
-        version = "0.1.0";
+        version = "0.2.0";
 
         src = lib.fileset.toSource {
           root = ./.;
